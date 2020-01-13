74b6bce553c553ce96a89e94db16781b604cd1c9
Scott Page <spage@godaddy.com>
2020-01-12 17:42:37 -0700

    Add code for sending fax.

--- app/send_fax.py
+++ app/send_fax.py
@@ -1,13 +1,73 @@
+# TODO: Write code to generate presigned URL for S3 PDF object
+# TODO: Look up how to log to cloudwatch from Python
+# TODO: Test parameter store lookup
+
+
 from __future__ import print_function
 import boto3
 import json
 import urllib
-import os
+import time
 
+from twilio.rest import Client
+
+# Initialize AWS clients
 s3_client = boto3.client('s3')
+ssm_client = boto3.client('ssm')
 
-# Get the table name from the Lambda Environment Variable
-phone_number = os.environ['PHONE_NUMBER']
+
+# Fetch all parameters
+account_sid = ssm_client.get_paramter(Name='account_sid')
+auth_token = ssm_client.get_paramter(Name='auth_token')
+to_number = ssm_client.get_paramter(Name='to_number')
+from_number = ssm_client.get_paramter(Name='from_number')
+
+
+def send_fax(self, url):
+    '''
+    Function for sending a fax
+    '''
+    client = Client(account_sid, auth_token)
+    fax = client.fax.faxes.create(
+        from_=from_number,
+        to=to_number,
+        quality="standard",
+        media_url=url
+    )
+    timeout = time.time() + 60*5   # 5 minutes from now
+    completed = False
+    status = "started"
+    while not completed or time.time() < timeout:
+        # Check status based on status listed on Twilio's site:
+        # https://www.twilio.com/docs/fax/api/faxes#fax-status-values
+        status = fax.status
+        if status in ["delivered", "no-answer", "busy", "failed", "canceled"]:
+            completed = True
+        else:
+            time.sleep(5)
+    if status == "delivered":
+        print(
+            "SUCCESS: Sending fax completed successfully."
+            "fax_id = {fax.sid}"
+            "fax_from = {fax.from_}"
+            "fax_to = {fax.to}"
+            "fax_quality = {fax.quality}"
+            "fax_num_pages = {fax.num_pages}"
+            "fax_duration = {fax.duration}"
+            "fax_price = {fax.price}"
+            "fax_price_unit = {fax.price_unit}"
+            "fax_date_created = {fax.date_created}"
+        )
+        return True
+    elif status == "started":
+        print("FAILED: Sending fax timed out while waiting for Twilio service to initialize.")
+        return False
+    elif status in ["queued", "processing", "sending"]:
+        print("FAILED: Sending fax timed out with status code: {}.")
+        return False
+    else:
+        print("FAILED: Sending fax failed with status code: {}".format(self.status))
+        return False
 
 
 # --------------- Main handler ------------------
@@ -24,6 +84,10 @@ def lambda_handler(event, context):
 
     try:
         print(bucket, key)
+        print("{account_sid}")
+        print("{auth_token}")
+        print("{to_number}")
+        print("{from_number}")
     except Exception as e:
         print("Error processing object {} from bucket {}. Event {}".format(key, bucket, json.dumps(event, indent=2)))
         raise e
